b2b5024d6242d93806ae355e14f5b5f28ebe0c2f
spemer <ghsspower@naver.com>
2018-09-11 18:10:20 +0900

    ENH ripple effect

--- js/src/index.js
+++ js/src/index.js
@@ -28,6 +28,85 @@
 
 
 /* ==============================
+    ripple effect
+============================== */
+(function(){
+    var getBtnTags = Array.prototype.slice.call(document.querySelectorAll('button'));
+    var getBtnNewPost = Array.prototype.slice.call(document.querySelectorAll('.header-newpost'));
+    var getBtnSidebar = Array.prototype.slice.call(document.querySelectorAll('.sidebar-item'));
+
+    var getBtnAll = [];
+    getBtnAll = getBtnAll.concat(getBtnTags, getBtnNewPost, getBtnSidebar);
+
+    getBtnAll.forEach(function(btn){
+        btn.setAttribute("ripple", "ripple");
+    });
+})();
+
+
+
+/* ==============================
+    ripple effect
+============================== */
+(function() {
+    var cleanUp,
+        debounce,
+        i,
+        len,
+        ripple,
+        rippleContainer,
+        ripples,
+        showRipple;
+
+    debounce = function(func, delay) {
+        var inDebounce;
+        inDebounce = undefined;
+        return function() {
+            var args, context;
+            context = this;
+            args = arguments;
+            clearTimeout(inDebounce);
+            return inDebounce = setTimeout(function() {
+            return func.apply(context, args);
+            }, delay);
+        };
+    };
+
+    showRipple = function(e) {
+        var pos, ripple, rippler, size, style, x, y;
+        ripple = this;
+        rippler = document.createElement('span');
+        size = ripple.offsetWidth;
+        pos = ripple.getBoundingClientRect();
+        x = e.pageX - pos.left - (size / 2);
+        y = e.pageY - pos.top - (size / 2);
+        style = 'top:' + y + 'px; left: ' + x + 'px; height: ' + size + 'px; width: ' + size + 'px;';
+        ripple.rippleContainer.appendChild(rippler);
+        return rippler.setAttribute('style', style);
+    };
+
+    cleanUp = function() {
+        while (this.rippleContainer.firstChild) {
+            this.rippleContainer.removeChild(this.rippleContainer.firstChild);
+        }
+    };
+
+    ripples = document.querySelectorAll('[ripple]');
+
+    for (i = 0, len = ripples.length; i < len; i++) {
+        ripple = ripples[i];
+        rippleContainer = document.createElement('div');
+        rippleContainer.className = 'ripple--container';
+        ripple.addEventListener('mousedown', showRipple);
+        ripple.addEventListener('mouseup', debounce(cleanUp, 2000));
+        ripple.rippleContainer = rippleContainer;
+        ripple.appendChild(rippleContainer);
+    }
+})();
+
+
+
+/* ==============================
     getFullYear
 ============================== */
 (function(){

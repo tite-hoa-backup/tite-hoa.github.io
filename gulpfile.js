21592bd02f6ceb0e86d4d76bf6bedfc6befe1625
spemer <ghsspower@naver.com>
2018-09-11 19:30:27 +0900

    CLN remove polyfill.js

--- gulpfile.js
+++ gulpfile.js
@@ -98,7 +98,6 @@ gulp.task('inject', function () {
                             './html/*.html',
                             './html/**/*.html']);
     var sources = gulp.src(['./js/dist/index.js',
-                            './js/dist/polyfill.js',
                             './css/dist/*.css'], {read: false});
 
     return target.pipe(inject(sources))
